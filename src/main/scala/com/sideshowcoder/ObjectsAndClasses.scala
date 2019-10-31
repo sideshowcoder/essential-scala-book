42f192276f7baf115823dc05f4bfc6ffc001f97e
Philipp Fehre <philipp@fehre.co.uk>
2019-10-31 15:10:01 +0000

    c3

---
+++ src/main/scala/com/sideshowcoder/ObjectsAndClasses.scala
@@ -0,0 +1,73 @@
+case class Person(firstName: String, lastName: String) {
+  def name =
+    s"$firstName $lastName"
+}
+
+object Stormtrooper {
+  def inspect(person: Person) =
+    person match {
+      case Person("Luke", "Skywalker") => "Stop!"
+      case Person("Han", "Solo") => "Stop!"
+      case Person(first, _) => s"Move along $first"
+    }
+}
+
+object alien {
+  def greet(p: Person) =
+    "Greetings " + p.name
+
+  def greetByName(first: String = "Some", last: String = "Body") =
+    "Greetings " + first + " " + last + "!"
+}
+
+object badassery {
+  def badass = throw new Exception("Error")
+  def other = null
+}
+
+case class Cat(name: String, color: String, food: String)
+
+object cats {
+  val oswald = Cat("Oswald", "black", "milk")
+  val henderson = Cat("Henderson", "ginger", "chips")
+  val quentin = Cat("Quentin", "tabby", "curry")
+}
+
+object ChipShop {
+  def willServe(cat: Cat) =
+    cat match {
+      case Cat(_, _, "chips") => true
+      case _ => false
+    }
+}
+
+class Director(val yearOfBirth: Int)
+
+class Film(val yearOfRelease: Int, val director: Director) {
+  def directorAge =
+    yearOfRelease - director.yearOfBirth
+
+  def copy(yOR: Int = yearOfRelease, d: Director = director) =
+    new Film(yOR, d)
+}
+
+case class Adder(x: Int) {
+  def apply(y: Int) =
+    y + x
+}
+
+case class Counter(count: Int = 0) {
+  def dec: Counter = dec()
+  def inc: Counter = inc()
+  def dec(n: Int = 1) = copy(count = count - n)
+  def inc(n: Int = 1) = copy(count = count + n)
+  def adjust(adder: Adder) = copy(count = adder(count))
+}
+
+
+class Timestamp(val secs: Long)
+
+object Timestamp {
+  def apply(hs: Int, mins: Int, secs: Int) =
+    new Timestamp(hs * 3600 + mins * 60 + secs)
+}
